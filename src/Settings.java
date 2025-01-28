fc7a8f84ef594d6557920ab52d95cf5604b63a9c
volodymyr <vvavodymyr@gmail.com>
2025-01-28 21:40:01 +0100

    Animal and Plant Characteristics have been added to the settings

--- src/Settings.java
+++ src/Settings.java
@@ -1,6 +1,84 @@
 public class Settings {
-
+  // НАСТОРОЙКИ РАЗМЕРА ОСТРОВА (КОЛ-ВО ЛОКАЦИЙ)
   public final static int columnsCount = 100;
   public final static int rowsCount = 1000;
+  // Характеристики животных:
+  public final static int wolfWeight = 50;
+  public final static int wolfMaxNumber = 30;
+  public final static int wolfSpeed = 50;
+  public final static int wolfSaturation = 50;
 
+  public final static int snakeWeight = 15;
+  public final static int snakeMaxNumber = 30;
+  public final static int snakeSpeed = 1;
+  public final static int snakeSaturation = 3;
+
+  public final static int foxWeight = 8;
+  public final static int foxMaxNumber = 30;
+  public final static int foxSpeed = 2;
+  public final static int foxSaturation = 2;
+
+  public final static int bearWeight = 500;
+  public final static int bearMaxNumber = 5;
+  public final static int bearSpeed = 2;
+  public final static int bearSaturation = 80;
+
+  public final static int eagleWeight = 6;
+  public final static int eagleMaxNumber = 20;
+  public final static int eagleSpeed = 3;
+  public final static int eagleSaturation = 1;
+
+  public final static int horseWeight = 400;
+  public final static int horseMaxNumber = 20;
+  public final static int horseSpeed = 4;
+  public final static int horseSaturation = 60;
+
+  public final static int deerWeight = 300;
+  public final static int deerMaxNumber = 20;
+  public final static int deerSpeed = 4;
+  public final static int deerSaturation = 50;
+
+  public final static int rabbitWeight = 2;
+  public final static int rabbitMaxNumber = 150;
+  public final static int rabbitSpeed = 2;
+  public final static double rabbitSaturation = 0.45;
+
+  public final static double mouseWeight = 0.05;
+  public final static int mouseMaxNumber = 500;
+  public final static int mouseSpeed = 1;
+  public final static double mouseSaturation = 0.01;
+
+  public final static int goatWeight = 60;
+  public final static int goatMaxNumber = 140;
+  public final static int goatSpeed = 3;
+  public final static int goatSaturation = 10;
+
+  public final static int sheepWeight = 70;
+  public final static int sheepMaxNumber = 140;
+  public final static int sheepSpeed = 3;
+  public final static int sheepSaturation = 15;
+
+  public final static int boarWeight = 400;
+  public final static int boarMaxNumber = 50;
+  public final static int boarSpeed = 2;
+  public final static int boarSaturation = 50;
+
+  public final static int buffaloWeight = 400;
+  public final static int buffaloMaxNumber = 50;
+  public final static int buffaloSpeed = 2;
+  public final static int buffaloSaturation = 50;
+
+  public final static int duckWeight = 1;
+  public final static int duckMaxNumber = 200;
+  public final static int duckSpeed = 4;
+  public final static double duckSaturation = 0.15;
+
+  public final static double caterpillarWeight = 0.01;
+  public final static int caterpillarMaxNumber = 1000;
+  public final static int caterpillarSpeed = 0;
+  public final static int caterpillarSaturation = 0;
+
+  // Растения
+  public final static double plantWeight = 1;
+  public final static int plantMaxNumber = 200;
 }
